b2fb6e9a4de7493ffce75c843e5b9193a762db88
Daniel-T-Kline <danieltk@umich.edu>
2024-02-08 14:17:24 -0500

    hello from me

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello from Daniel");
         for(int i=1;i<=10;i++) {
-            System.out.println("Hello World from John and Nick and Daniel..."+i);
+            System.out.println("Hello World from John and Nick..."+i);
         }
     }
 }
