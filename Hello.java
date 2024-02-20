391d7191a8c0efdadd69057870aa9a141d18e5b4
Nick Harrison <nharr@umich.edu>
2024-02-20 14:33:02 -0500

    testing CI

--- Hello.java
+++ Hello.java
@@ -2,8 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
 
+        System.out.println("Testing continuous integration...");
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World from John and Nick..."+i);
+
         }
     }
 }
